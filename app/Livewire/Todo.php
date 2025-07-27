a05eef4aba37dac03b147d21e53800c42c110685
Alrak <lekanraheem827@gmail.com>
2025-07-27 09:11:20 -0700

    create todo finished

--- app/Livewire/Todo.php
+++ app/Livewire/Todo.php
@@ -2,10 +2,24 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use App\Models\Todo as TodoModel;
 
 class Todo extends Component
-{
+{   
+    #[Rule("required|string|max:100")]
+    public $name;
+
+    public function create(){
+        $validated = $this->validateOnly("name");
+
+        TodoModel::create($validated);
+        $this->reset("name");
+        session()->flash('success', "Todo created");
+
+    }
+
     public function render()
     {
         return view('livewire.todo');
